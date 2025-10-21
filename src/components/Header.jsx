0daf8daa139c5f26f1a5e363069104b9dba8d5fd
Gregor Cool <willytk249@gmail.com>
2025-10-21 12:52:19 -0400

    inclass

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import './../css/Header.css';
+
+const Header = () => {
+    return (
+        <header id="main-header">
+            <h1>Header</h1>
+        </header>
+    );
+};
+
+
+export default Header;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import "./../css/Home.css";
+
+const Home = () => {
+    return (
+        //Apply id to it otherwise style h1 applies to all.
+        <div id="home">
+            <h1>I love home</h1>
+            <h2>Home is for the heart</h2>
+        </div>
+    );
+};
+
+export default Home;
